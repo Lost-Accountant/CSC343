cdcd58b4551fb6bd30f2af0f4b455a0eac345327
Lost-Accountant <gxsgtghost141@gmail.com>
2021-04-08 22:53:22 -0400

    Fixed bug in creating view. TODO: select >30% (group by country_id, count(pair)/count(id) >30%?)

--- data/q1.sql
+++ data/q1.sql
@@ -1,8 +1,8 @@
-
-SELECT es1.id, es1.party_id, es2.party_id, es1.election_id
+CREATE VIEW alliance AS(
+SELECT es1.id, es1.party_id AS leader_id, es2.party_id AS follower_id, es1.election_id
 FROM parlgov.election_result AS es1, parlgov.election_result AS es2
-WHERE es1.id = es2.alliance_id;
+WHERE es1.id = es2.alliance_id);
 
-SELECT country_id, leader_id, follower_id, t2.id
+SELECT t2.country_id, leader_id, follower_id, t2.id
 FROM alliance, parlgov.election AS t2
 WHERE alliance.election_id = t2.id;
