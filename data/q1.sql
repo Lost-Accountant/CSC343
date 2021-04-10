fe4384e9bb463f64b1aab3ddbb7faa112ca163cd
Lost-Accountant <gxsgtghost141@gmail.com>
2021-04-10 12:50:26 -0400

    Need to find a way to get the 30% work

--- data/q1.sql
+++ data/q1.sql
@@ -1,13 +1,25 @@
-CREATE VIEW alliance AS(
-SELECT es1.id, es1.party_id AS leader_id, es2.party_id AS follower_id, es1.election_id
-FROM parlgov.election_result AS es1, parlgov.election_result AS es2
-WHERE es1.id = es2.alliance_id);
+DROP TABLE IF EXISTS parlgov.Q1 CASCADE;
+DROP VIEW IF EXISTS alliance CASCADE;
 
-SELECT t2.country_id, leader_id, follower_id, t2.id
-FROM alliance, parlgov.election AS t2
-WHERE alliance.election_id = t2.id;
+CREATE TABLE parlgov.Q1(
+countryId INTEGER,
+alliedPartyId1 INTEGER,
+alliedPartyId2 INTEGER,
+PRIMARY KEY (alliedPartyId1, alliedPartyId2));
 
-SELECT DISTINCT t3.name
-FROM alliance, parlgov.election AS t2, parlgov.country AS t3
-WHERE alliance.election_id = t2.id AND t2.country_id = t3.id;
--- can see that only 3 countries have those alliance stuff.
+CREATE VIEW alliance AS (
+SELECT es1.party_id AS leader_id, es2.party_id AS follower_id,
+es1.election_id, e1.country_id,
+-- preserve election_id for future count
+CAST(es1.party_id AS VARCHAR(10)) || CAST(es2.party_id AS VARCHAR(10)) AS pair
+-- combine leader and follower as pair for later use
+FROM parlgov.election_result AS es1, parlgov.election_result AS es2,
+parlgov.election AS e1
+WHERE es1.id = es2.alliance_id AND es1.election_id = e1.id
+-- match alliance and match country for each election
+);
+
+SELECT country_id, COUNT(DISTINCT election_id)
+FROM alliance
+GROUP BY country_id
+-- get number of elections per country
