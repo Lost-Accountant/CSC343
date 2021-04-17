b7064bec3c5229ecd530ff443988ca73e72a0b98
Lost-Accountant <gxsgtghost141@gmail.com>
2021-04-17 17:06:49 -0400

    TODO: find out how ideal - reality works in SQL

--- q2.sql
+++ q2.sql
@@ -1,11 +1,14 @@
 DROP VIEW IF EXISTS parlgov.AllCabinetParty20 CASCADE
+
 DROP VIEW IF EXISTS parlgov.reality CASCADE
+
+DROP VIEW IF EXISTS parlgov.ideal CASCADE
 
 CREATE VIEW parlgov.AllCabinetParty20 AS (
 SELECT t1.id AS party_id, t3.id AS cabinet_id, t3.country_id AS country_id
 FROM parlgov.party AS t1, parlgov.cabinet_party AS t2, parlgov.cabinet AS t3
 WHERE t1.id = t2.party_id AND t2.cabinet_id = t3.id AND t3.start_date >= '2001-01-01'
-)
+);
 -- select all parties that were in the cabinets that started after 2001.
 -- means that the view will have all parties that were in a cabinet sometime
 -- during the past 20 years
@@ -25,5 +28,22 @@ ON t1.party_id = t3.party_id
 
 SELECT *
 FROM parlgov.reality
+WHERE country_id = 54
 
--- TODO: Do that planned move on note
+CREATE VIEW ideal AS (
+SELECT DISTINCT t2.party_id, t1.cabinet_id, t1.country_id
+FROM parlgov.reality AS t1
+CROSS JOIN parlgov.AllCabinetParty20 AS t2
+WHERE t1.country_id = t2.country_id
+-- make sure joining from same country
+-- create ideal situation where party from one country is in every cabinet of that country
+);
+
+SELECT *
+FROM ideal
+WHERE country_id = 54
+
+-- TODO: Find out how ideal - reality works in SQL
+-- It is not ideal - reality (looks like working but not actually)
+-- Nor is ideal NOT EXISTS (* reality), get empty return
+
