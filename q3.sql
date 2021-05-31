0ad8e0aaddbc5e348a3a363ced84f35916066e41
Lost-Accountant <gxsgtghost141@gmail.com>
2021-05-30 23:00:26 -0400

    Q3 almost done.
    TODO: link party id to party name and family, and link country id to country name, and combine it with MostRecentWon

--- q3.sql
+++ q3.sql
@@ -2,9 +2,11 @@ DROP VIEW IF EXISTS WonElection CASCADE;
 DROP VIEW IF EXISTS CountWon CASCADE;
 DROP VIEW IF EXISTS ElectionAverage CASCADE;
 DROP VIEW IF EXISTS ExcelParty CASCADE;
+DROP VIEW IF EXISTS MostRecentWon CASCADE;
 
+-- elections won for every party in every nation
 CREATE VIEW WonElection AS (
-SELECT t1.party_id, t2.country_id, t1.election_id, DATE_PART('year', t2.e_date) AS election_year
+SELECT t1.party_id, t2.country_id, t1.election_id, DATE_PART('year', t2.e_date) AS election_year, t2.e_date as election_date
 FROM parlgov.election_result AS t1, parlgov.election AS t2
 WHERE t1.election_id = t2.id AND t1.seats > 0
 );
@@ -19,11 +21,27 @@ CREATE VIEW ElectionAverage AS (
 SELECT country_id, AVG(won_count) AS avg_won
 FROM CountWon
 GROUP BY country_id
-)
+);
 
+-- Select parties that won more than 3 times average
 CREATE VIEW ExcelParty AS (
 SELECT t1.country_id, t1.party_id, t1.won_count
 FROM CountWon AS t1, ElectionAverage AS t2
 WHERE t1.country_id = t2.country_id
 AND t1.won_count >= 3 * avg_won
 );
+
+SELECT* FROM ExcelParty;
+
+-- Find the latest election won for a given party
+CREATE VIEW MostRecentWon AS
+SELECT party_id, election_id AS mostRecentlyWonElectionId, MAX(election_year) AS mostRecentlyWonElectionYear
+FROM WonElection
+GROUP BY party_id, election_id
+WHERE election_date IN (
+-- the date selected is the most recent won one for the party
+    SELECT MAX(election_date) AS mostRecentlyWonElectionYear
+    FROM WonElection
+    GROUP BY party_id
+    )
+;
