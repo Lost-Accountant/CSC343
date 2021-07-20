7285d7d4893f9deb13a54b342552f4f069ad8bc7
Lost-Accountant <gxsgtghost141@gmail.com>
2021-07-19 22:05:23 -0400

    Q5 finished.
    TODO: Q6

---
+++ q5-order.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM parlgov.q5
+ORDER BY countryName DESC, year DESC;
--- q5.sql
+++ q5.sql
@@ -1,5 +1,6 @@
 DROP VIEW IF EXISTS AllRatios CASCADE;
 DROP VIEW IF EXISTS AllAvgRatios CASCADE;
+DROP TABLE IF EXISTS parlgov.q5 CASCADE;
 
 CREATE VIEW AllRatios AS (
 SELECT DATE_PART('year', e_date) AS year, id, country_id, CAST(votes_cast AS DECIMAL)/electorate AS participation
@@ -9,7 +10,6 @@ AND votes_cast IS NOT NULL
 AND electorate IS NOT NULL
 );
 
-SELECT * FROM AllRatios;
 
 CREATE VIEW AllAvgRatios AS (
 SELECT year, country_id, AVG(participation) AS participationRatio
@@ -17,3 +17,16 @@ FROM AllRatios
 GROUP BY country_id, year
 );
 
+--Remove countries with participation ratio decreasing
+-- No result as all countries had experienced decrease
+CREATE TABLE parlgov.q5
+AS
+SELECT t2.name AS countryName, t1.year, t1.participationRatio
+FROM AllAvgRatios AS t1, Country AS t2
+WHERE t1.country_id = t2.id
+AND NOT EXISTS (
+SELECT *
+FROM AllAvgRatios AS t1, AllAvgRatios AS t2
+WHERE t1.country_id = t2.country_id
+AND t1.year <= t2.year
+AND t1.participationRatio > t2.participationRatio);
