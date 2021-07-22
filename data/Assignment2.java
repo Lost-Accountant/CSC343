32ffb61d319172212051ea721ff061610b1b5203
Lost-Accountant <gxsgtghost141@gmail.com>
2021-07-21 22:32:03 -0400

    Part 2 started. Unhandled class exception???

--- data/Assignment2.java
+++ data/Assignment2.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 import java.util.List;
+import java.util.Scanner;
 
 // If you are looking for Java data structures, these are highly useful.
 // Remember that an important part of your mark is for doing as much in SQL (not Java) as you can.
@@ -12,26 +13,33 @@ import java.util.List;
 public class Assignment2 extends JDBCSubmission {
 
     public Assignment2() throws ClassNotFoundException {
-
         Class.forName("org.postgresql.Driver");
     }
 
     @Override
     public boolean connectDB(String url, String username, String password) {
         // Implement this method!
+        String fullUrl = "jdbc:postgresql://" + url + ":5432/CSC343";
         try {
-            Connection conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connection established");
-            return true;
+            connection = DriverManager.getConnection(fullUrl, username, password);
+            System.out.println("Connection successful");
         } catch (SQLException e){
-            return false;
+            System.out.println("Failed to estabilish connection");
+            e.printStackTrace();
         }
+        return true;
     }
 
     @Override
     public boolean disconnectDB() {
         // Implement this method!
-        return false;
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Failed to close connection");
+            e.printStackTrace();
+        }
+        return true;
     }
 
     @Override
@@ -49,6 +57,19 @@ public class Assignment2 extends JDBCSubmission {
     public static void main(String[] args) {
         // You can put testing code in here. It will not affect our autotester.
         System.out.println("Hello");
+        // test connection to databse
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter database IP:");
+        String ip = userInput.next();
+
+        System.out.println("Please enter username:");
+        String username = userInput.next();
+
+        System.out.println("Please enter password");
+        String password = userInput.next();
+
+        Assignment2 frame = new Assignment2();
+        frame.connectDB(ip, username, password);
     }
 
 }
