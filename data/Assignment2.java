c5fcc5c716fede2c0ff31865d0c95fd09c42f46d
Lost-Accountant <gxsgtghost141@gmail.com>
2021-07-26 22:36:19 -0400

    Part 2 started. JDBC driver still can't be loaded somehow.

--- data/Assignment2.java
+++ data/Assignment2.java
@@ -21,11 +21,12 @@ public class Assignment2 extends JDBCSubmission {
         // Implement this method!
         String fullUrl = "jdbc:postgresql://" + url + ":5432/CSC343";
         try {
-            connection = DriverManager.getConnection(fullUrl, username, password);
+            this.connection = DriverManager.getConnection(fullUrl, username, password);
             System.out.println("Connection successful");
         } catch (SQLException e){
-            System.out.println("Failed to estabilish connection");
+            System.out.println("Failed to establish connection");
             e.printStackTrace();
+            return false;
         }
         return true;
     }
@@ -38,6 +39,7 @@ public class Assignment2 extends JDBCSubmission {
         } catch (SQLException e) {
             System.out.println("Failed to close connection");
             e.printStackTrace();
+            return false;
         }
         return true;
     }
@@ -45,12 +47,37 @@ public class Assignment2 extends JDBCSubmission {
     @Override
     public ElectionCabinetResult electionSequence(String countryName) {
         // Implement this method!
+        String query = "SELECT t1.id AS election_id, DATE_PART('year', t1.e_date) AS year,\n" +
+                "t3.id AS current_cabinet, t3.previous_cabinet_id AS previous_cabinet\n" +
+                "FROM election AS t1, country AS t2, cabinet AS t3\n" +
+                "WHERE t1.country_id = t2.id AND t1.id = t3.election_id\n" +
+                "AND t2.name = ? \n" +
+                "ORDER BY t1.e_date DESC;\n";
+        try {
+            PreparedStatement execstat = connection.prepareStatement(query);
+            execstat.setString(1, countryName);
+            // get result, rs iterable
+            ResultSet rs = execstat.executeQuery();
+            System.out.println(rs);
+        }
+        catch(SQLException e) {
+            System.out.println("Failed to get cabinet result." + e.getMessage());
+            return null;
+        }
         return null;
     }
 
     @Override
     public List<Integer> findSimilarPoliticians(Integer politicianName, Float threshold) {
         // Implement this method!
+        try {
+            PreparedStatement execstat = connection.prepareStatement("SELECT * FROM country");
+            execstat.executeQuery();
+        }
+        catch(SQLException e) {
+            System.out.println("Failed to get cabinet result." + e.getMessage());
+            return null;
+        }
         return null;
     }
 
@@ -68,9 +95,13 @@ public class Assignment2 extends JDBCSubmission {
         System.out.println("Please enter password");
         String password = userInput.next();
 
-        Assignment2 frame = new Assignment2();
-        frame.connectDB(ip, username, password);
+        try {
+            Assignment2 frame = new Assignment2();
+            frame.connectDB(ip, username, password);
+        }
+        catch(ClassNotFoundException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        }
     }
-
-}
-
